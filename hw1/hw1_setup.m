6b355695dfb5819839287d88ac01b592946dda6a
Robert Jones <rjjones@umich.edu>
2023-10-04 11:37:37 -0400

    Create hw1_setup.m

---
+++ hw1/hw1_setup.m
@@ -0,0 +1,11 @@
+% Change the path below to the path containing /hw1, then run this script to add the necessary paths to your Matlab search path.
+
+codedir = '/hw1';
+
+%%%% Add all dirs in /hw1 to path
+addpath(genpath(codedir));
+
+%%%% Alternatively, just add these necessary paths:
+% addpath([codedir filesep 'utils']);
+% addpath([codedir filesep 'scripts']);
+% addpath([codedir filesep 'plots']); *)
